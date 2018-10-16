b9b2b61b68cecd94a15a20655ccd661f3936bdd8
nspindel <nspindel@bio.fsu.edu>
2018-10-15 20:41:00 -0400

    Initial commit

---
+++ functions.R
@@ -0,0 +1,13 @@
+# ipak function: install and load multiple R packages.  check to see if
+# packages are installed. Install them if they are not, then load them
+# into the R session.
+# SOURCE: https://gist.github.com/stevenworthington/3178163
+# argument: pkg
+ipak <- function(pkg) {
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+# usage: packages <- c('RMySQL', 'ggplot2', 'gridExtra', 'reshape2',
+# 'scales', 'grid') ipak(packages)
